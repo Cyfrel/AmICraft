6cd73c6282639b8fb166a5b7f54b718f1b1cd690
Geraldo Fulgencio <57548348+Cyfrel@users.noreply.github.com>
2022-06-25 14:28:31 -0300

    Update trocaimg.js
    Aprimorada seção itens e Adicionadas seções tier/enchants

--- trocaimg.js
+++ trocaimg.js
@@ -1,63 +1,126 @@
-function a1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+// ITENS
+function item1(){
+    document.getElementById("trocarimgitem").src="itens/a1.png";
 }
 
-function a2(){
-    document.getElementById("trocarimg").src="itens/a2.png";
+function item2(){
+    document.getElementById("trocarimgitem").src="itens/a2.png";
 }
 
-function b1(){
-    document.getElementById("trocarimg").src="itens/b1.png";
+function item3(){
+    document.getElementById("trocarimgitem").src="itens/b1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item4(){
+    document.getElementById("trocarimgitem").src="itens/b2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item5(){
+    document.getElementById("trocarimgitem").src="itens/f1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item6(){
+    document.getElementById("trocarimgitem").src="itens/f2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item7(){
+    document.getElementById("trocarimgitem").src="itens/g1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item8(){
+    document.getElementById("trocarimgitem").src="itens/g2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item9(){
+    document.getElementById("trocarimgitem").src="itens/h1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item10(){
+    document.getElementById("trocarimgitem").src="itens/h2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item11(){
+    document.getElementById("trocarimgitem").src="itens/m1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item12(){
+    document.getElementById("trocarimgitem").src="itens/m2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item13(){
+    document.getElementById("trocarimgitem").src="itens/n1.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item14(){
+    document.getElementById("trocarimgitem").src="itens/n2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item15(){
+    document.getElementById("trocarimgitem").src="itens/a2.png";
 }
 
-function adg1(){
-    document.getElementById("trocarimg").src="itens/a1.png";
+function item16(){
+    document.getElementById("trocarimgitem").src="itens/b2.png";
+}
+
+function item17(){
+    document.getElementById("trocarimgitem").src="itens/f2.png";
+}
+
+function item18(){
+    document.getElementById("trocarimgitem").src="itens/g2.png";
+}
+
+function item19(){
+    document.getElementById("trocarimgitem").src="itens/h2.png";
+}
+
+function item20(){
+    document.getElementById("trocarimgitem").src="itens/m2.png";
+}
+
+function item21(){
+    document.getElementById("trocarimgitem").src="itens/n2.png";
+}
+
+
+
+// TIERS
+function tier4(){
+    document.getElementById("trocarimgtier").src="tiers/t4.png";
+}
+
+function tier5(){
+    document.getElementById("trocarimgtier").src="tiers/t5.png";
+}
+
+function tier6(){
+    document.getElementById("trocarimgtier").src="tiers/t6.png";
+}
+
+function tier7(){
+    document.getElementById("trocarimgtier").src="tiers/t7.png";
+}
+
+function tier8(){
+    document.getElementById("trocarimgtier").src="tiers/t8.png";
+}
+
+
+// ENCHANTS
+
+function enchant0(){
+    document.getElementById("trocarimgenchant").src="enchant/0.png";
+}
+
+function enchant1(){
+    document.getElementById("trocarimgenchant").src="enchant/1.png";
+}
+
+function enchant2(){
+    document.getElementById("trocarimgenchant").src="enchant/2.png";
+}
+
+function enchant3(){
+    document.getElementById("trocarimgenchant").src="enchant/3.png";
 }
