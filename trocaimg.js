f9bfdd93fc0611c9a0cfa8de733f8a5929a88de3
Geraldo Fulgencio <57548348+Cyfrel@users.noreply.github.com>
2022-06-25 02:37:03 -0300

    Create trocaimg.js

---
+++ trocaimg.js
@@ -0,0 +1,63 @@
+function a1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function a2(){
+    document.getElementById("trocarimg").src="itens/a2.png";
+}
+
+function b1(){
+    document.getElementById("trocarimg").src="itens/b1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
+
+function adg1(){
+    document.getElementById("trocarimg").src="itens/a1.png";
+}
